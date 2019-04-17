45981e0aecd159d133c9bc3d954c4326a89cfbdd
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-04-17 12:31:56 +0300

    Update Guesser

--- Guesser.hpp
+++ Guesser.hpp
@@ -9,11 +9,20 @@ namespace bullpgia {
 class Guesser {
 
 public:
+    int index;
+    string temp="";
     unsigned int length;
     std::string _guess;
+    int ans[10];
     virtual std::string guess() = 0;
     void learn(std::string _guess) { this->_guess = _guess; }
-    void startNewGame(unsigned int length) { this->length = length; }
+    void startNewGame(unsigned int length) {this->length = length; index = 0;  
+    for (size_t i = 0; i < 10; i++)
+    {
+        ans[i]=0;
+    }
+    
+    }
 
 
 };
