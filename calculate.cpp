fcf2ad1170cf9243eef42ba90f6335c46f38f940
Or Abuhtzura <44296674+orabu103@users.noreply.github.com>
2019-04-17 12:31:22 +0300

    Update calculate

--- calculate.cpp
+++ calculate.cpp
@@ -1,12 +1,38 @@
 
 #include "calculate.hpp"
+#include <iostream>
+using namespace std;
 
 
 std::string calculateBullAndPgia(std::string s, std::string t) {
-    if(s.compare(t) ==0){
+    int bull=0;
+    int pgia=0;
+    string ans ="";
+    string temp = t;
+
+    if(s.compare(t)==0){
         return "4,0";
     }
-    else return "0,4";
-    
-     
-     }
+    else {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if(s.at(i)==temp.at(i)){
+                    bull++;
+                }
+            else{
+                for (int j = 0; j < t.length(); j++)
+                {
+                    if(s.at(i)==temp.at(j) && s.at(j)!=temp.at(j))
+                    {
+                        pgia++;
+                        temp.at(j)='x';
+                    }
+                }
+            }
+        }
+        ans+=std::to_string(bull)+","+std::to_string(pgia);
+        // cout << ans << endl;
+
+        return ans;
+    }
+}
